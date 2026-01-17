d242202211442f193f4df6f597dab1c5bc5fe45e
Chad Clark <chad-git-p.superfrink@superfrink.net>
2026-01-17 11:53:07 -0700

    Remove unnecessary unveil calls from OpenBSD sandbox
    The pledge promises (stdio dns inet) don't include any filesystem
    access (rpath/wpath/cpath), so unveil restrictions have no effect.

--- cmd/go-cymru-asn/sandbox_openbsd.go
+++ cmd/go-cymru-asn/sandbox_openbsd.go
@@ -10,27 +10,6 @@ import (
 )
 
 func sandbox() {
-	unveilPaths := []struct {
-		path  string
-		perms string
-	}{
-		{"/etc/resolv.conf", "r"},
-		{"/etc/ssl/cert.pem", "r"},
-		{"/etc/ssl/certs", "r"},
-	}
-
-	for _, u := range unveilPaths {
-		if err := unix.Unveil(u.path, u.perms); err != nil {
-			fmt.Fprintf(os.Stderr, "unveil %s: %v\n", u.path, err)
-			os.Exit(1)
-		}
-	}
-
-	if err := unix.UnveilBlock(); err != nil {
-		fmt.Fprintf(os.Stderr, "unveil block: %v\n", err)
-		os.Exit(1)
-	}
-
 	if err := unix.Pledge("stdio dns inet", ""); err != nil {
 		fmt.Fprintf(os.Stderr, "pledge: %v\n", err)
 		os.Exit(1)
