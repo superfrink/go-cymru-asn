c6e514bcc0aee3e544c86e0c0d8803ebfec738de
Chad Clark <chad-git-p.superfrink@superfrink.net>
2026-01-17 11:07:25 -0700

    fix: .gitignore was blocking the cli directory.

---
+++ cmd/go-cymru-asn/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	cymruasn "github.com/superfrink/go-cymru-asn"
+)
+
+func main() {
+	timeout := flag.Duration("timeout", 30*time.Second, "connection timeout")
+	server := flag.String("server", cymruasn.DefaultServer, "whois server address")
+	flag.Parse()
+
+	ips := flag.Args()
+
+	if len(ips) == 0 {
+		ips = readFromStdin()
+	}
+
+	if len(ips) == 0 {
+		fmt.Fprintln(os.Stderr, "usage: go-cymru-asn [-timeout duration] [-server addr] IP [IP ...]")
+		fmt.Fprintln(os.Stderr, "       or pipe IPs via stdin (one per line)")
+		os.Exit(2)
+	}
+
+	client := cymruasn.NewClient(
+		cymruasn.WithTimeout(*timeout),
+		cymruasn.WithServer(*server),
+	)
+
+	resp, err := client.Lookup(context.Background(), ips)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(2)
+	}
+
+	for _, r := range resp.Results {
+		fmt.Printf("%s\t%d\t%s\t%s\t%s\n", r.IP, r.ASN, r.BGPPrefix, r.CountryCode, r.ASName)
+	}
+
+	for _, e := range resp.Errors {
+		fmt.Fprintf(os.Stderr, "error: %s: %v\n", e.IP, e.Err)
+	}
+
+	if len(resp.Errors) > 0 && len(resp.Results) > 0 {
+		os.Exit(1)
+	}
+
+	if len(resp.Errors) > 0 && len(resp.Results) == 0 {
+		os.Exit(2)
+	}
+}
+
+func readFromStdin() []string {
+	var ips []string
+
+	info, err := os.Stdin.Stat()
+	if err != nil {
+		return ips
+	}
+
+	if info.Mode()&os.ModeCharDevice != 0 {
+		return ips
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line != "" {
+			ips = append(ips, line)
+		}
+	}
+
+	return ips
+}
